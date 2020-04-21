2b30e4acb4471b1e7e36f15b22c7d247112a6c2b
CMonkey <42.codemonkey@gmail.com>
2020-04-21 17:43:57 +0800

    init project

---
+++ src/test/java/org/excavator/boot/graalvm/GraalvmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.excavator.boot.graalvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalvmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
